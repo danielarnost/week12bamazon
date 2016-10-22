eacb8267a0bc30cb89bfb533c11092c0ccbdbea4
danielarnost <danielarnost@utexaas.edu>
2016-10-22 09:22:20 -0500

    partone

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -24,4 +24,27 @@ conn.query('SELECT * FROM Products',function(err,rows){
 	
 	console.log("ID: " + rows[i].ItemID + ", Product: " + rows[i].ProductName + ", Department: " + rows[i].DepartmentName + ", Price: $" + rows[i].Price);
 	}
+		function productId() {
+		
+		var self = this;
+		prompt.get(['chooseProductID'], function(err, result) {
+			parseInt(result.chooseProductID);
+			result.chooseProductID--;
+			console.log('Add item: "' + rows[result.chooseProductID].ProductName + '" to Bamazon cart. Please choose your desired quantity. ');
+
+		prompt.get(['howManyUnits'], function(err, res) {
+			parseInt(res.howManyUnits);
+				parseInt(rows[result.chooseProductID].StockQuantity);
+				if (res.howManyUnits > rows[result.chooseProductID].StockQuantity) {
+					console.log('Sorry, Bamazon currently has an insufficient quantity to complete this order ');
+				}else {
+					console.log('You purchased ' + res.howManyUnits + " " + rows[result.chooseProductID].ProductName + 's for $' + rows[result.chooseProductID].Price * res.howManyUnits);
+					rows[result.chooseProductID].StockQuantity = rows[result.chooseProductID].StockQuantity - res.howManyUnits;
+					console.log('Remaining quantity: ' + rows[result.chooseProductID].StockQuantity)
+				}
+				productId();
+			});
+		});
+	}
+	productId();
 });
