9bc4a9079841893f5f95538882d2344b129ddaca
danielarnost <danielarnost@utexaas.edu>
2016-11-11 12:00:24 -0600

    jing screencast link in readme

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -19,7 +19,7 @@ conn.connect(function(err){
 conn.query('SELECT * FROM Products',function(err,rows){
 	if(err) throw err;
 
-	console.log('Data received from Db:\n');
+	console.log('Data received from DB:\n');
 	for (var i=0; i<rows.length; i++) {
 	
 	console.log("ID: " + rows[i].ItemID + ", Product: " + rows[i].ProductName + ", Department: " + rows[i].DepartmentName + ", Price: $" + rows[i].Price);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # week12bamazon
+
+http://screencast.com/t/Y01owleagMB0
